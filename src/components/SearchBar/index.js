7d7f9d7709b2953378db2211454db82777bdcaaf
joe960913 <59116882@qq.com>
2019-12-13 16:59:48 +0800

    SearchBar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
-export default function index() {
-    return (
-        <div>
-            
-        </div>
-    )
+import React from "react";
+import InputField from "../../elements/InputField";
+// import Button from "../../elements/Button";
+import styles from './index.less'
+export default function SearchBar(props) {
+  return (
+    <div className={styles.SearchBar}>
+      <InputField
+        type="text"
+        name={props.city}
+        onChange={props.onSearch}
+        error={props.error}
+        placeholder='Enter a city'
+        label='Location'
+        value={props.value}
+      />
+      {/* <Button onClick={props.goSearch} name='searchSubmit' tyoe='submit' position='onForm'>Search</Button> */}
+    </div>
+  );
 }
--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { MoonLoader } from "react-spinners";
 
-// import classes from "./App.less";
+ import styles from "./App.less";
 import assetMapping from "../../assets/assetMapping.json";
 // import Card from "../../elements/Card/Card";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import SearchBar from "../../components/SearchBar/SearchBar";
+import SearchBar from "../../components/SearchBar";
+import { resolve } from "url";
 // import WeatherDetails from "../../components/WeatherDetails/WeatherDetails";
 // import Preview from "../../components/Preview/Preview";
 // import ErrorNotice from "../../components/ErrorNotice/ErrorNotice";
@@ -28,21 +29,51 @@ const App = () => {
   let onSearch = e => {
     setInput(e.target.value);
   };
-  let goSearch = () => {};
+  let goSearch = () => {
+    const city = Input;
+    const api_key = process.env.REACT_WEATHER_API_KEY;
+    const api_url = "https://api.openweathermap.org/data/2.5/weather";
+    const url = api_url + `?q=${city}&appid=${api_key}&units=metric`;
+    new Promise(resolve => {
+      setWeatherDetails({});
+      setLoading(true);
+      setError(false);
+      resolve();
+    }).then(() => {
+      fetch(url)
+        .then(res => res.json)
+        .then(data => {
+          if (data.cod === 200) {
+            setWeatherDetails({
+              temperature: data.main.temp,
+              description: data.weather[0].main
+            });
+            setLoading(false);
+          } else {
+            throw data.cod;
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          setError(true);
+          setLoading(false);
+        });
+    });
+  };
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Header
         color={
           assetMapping.colors[error ? "error" : weatherDetails.description]
         }
         onClickHandler={tryAgain}
       />
-      <main>
+      <main className={styles.main}>
         <SearchBar
           value={Input}
           onChangeHandler={onSearch}
           onClickHandler={goSearch}
-          error
+          error={error}
         />
         {/* <Card /> */}
       </main>
