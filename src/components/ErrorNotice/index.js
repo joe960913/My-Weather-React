6be34fc4e941f1e11b1641622ee2ba262db06da9
joe960913 <59116882@qq.com>
2019-12-16 13:34:39 +0800

    chinese

--- src/components/ErrorNotice/index.js
+++ src/components/ErrorNotice/index.js
@@ -8,8 +8,8 @@ const errorNotice = (props) => {
         <div className={classes.ErrorNoticeWrapper}>
             <h1 className={classes.NotFound404}>404</h1>
             <div className={classes.ErrorTextWrapper}>
-                <h2 className={classes.NotFoundHeading}>Oops!</h2>
-                <p className={classes.NotFoundDetails}>We can't find the city you are looking for.</p>
+                <h2 className={classes.NotFoundHeading}>糟糕!</h2>
+                <p className={classes.NotFoundDetails}>找不到你想找的城市呀</p>
                 <Button 
                     name="tryAgain" 
                     type="button" 
