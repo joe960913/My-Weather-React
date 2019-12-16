2fdea02fe5983f537897655ffd1a4ff872d6003f
joe960913 <59116882@qq.com>
2019-12-16 10:44:42 +0800

    Updates

---
+++ precache-manifest.8a389b2b4513d854ed46472baaabace9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec3ec8a2e1562fbe8ed15c7b014adb1",
+    "url": "/My-Weather-React/index.html"
+  },
+  {
+    "revision": "eb2940f13f2a36fee83d",
+    "url": "/My-Weather-React/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "67e1762316cb41073f45",
+    "url": "/My-Weather-React/static/css/main.a83be363.chunk.css"
+  },
+  {
+    "revision": "eb2940f13f2a36fee83d",
+    "url": "/My-Weather-React/static/js/2.9027316d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/My-Weather-React/static/js/2.9027316d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67e1762316cb41073f45",
+    "url": "/My-Weather-React/static/js/main.b40c4c46.chunk.js"
+  },
+  {
+    "revision": "90d94fe01bb145e138f8",
+    "url": "/My-Weather-React/static/js/runtime-main.170036cc.js"
+  },
+  {
+    "revision": "f963a166b3279d455437165cb78cce6a",
+    "url": "/My-Weather-React/static/media/Clear.f963a166.svg"
+  },
+  {
+    "revision": "a82005aa954c61cba1a50336159d70d8",
+    "url": "/My-Weather-React/static/media/Cloud-wind.a82005aa.svg"
+  },
+  {
+    "revision": "a98369fb7ac4ac2d522c92642bb69bc8",
+    "url": "/My-Weather-React/static/media/Clouds.a98369fb.svg"
+  },
+  {
+    "revision": "1ca35fb1145fa93cb66fa81fc2b9da72",
+    "url": "/My-Weather-React/static/media/Drizzle.1ca35fb1.svg"
+  },
+  {
+    "revision": "e3158b56e90d7c4ae2ac85e0ef7ed4bc",
+    "url": "/My-Weather-React/static/media/Moon.e3158b56.svg"
+  },
+  {
+    "revision": "06f78ffe921aff71ae391ae110b95c08",
+    "url": "/My-Weather-React/static/media/Night-rainy.06f78ffe.svg"
+  },
+  {
+    "revision": "7d9c9233f8d2a0402a90753501bfd21e",
+    "url": "/My-Weather-React/static/media/Night.7d9c9233.svg"
+  },
+  {
+    "revision": "29a0aca0c7b11b9e2e20e203bca66089",
+    "url": "/My-Weather-React/static/media/Preview.29a0aca0.svg"
+  },
+  {
+    "revision": "b5e8855f869aeaa037290f87cf347fd8",
+    "url": "/My-Weather-React/static/media/Rain.b5e8855f.svg"
+  },
+  {
+    "revision": "104ea80ade6e3fd46928a77892b6d86c",
+    "url": "/My-Weather-React/static/media/Snow.104ea80a.svg"
+  },
+  {
+    "revision": "c623741c0b9e188e5b6a93a5e1c0ad9e",
+    "url": "/My-Weather-React/static/media/Thunderstorm.c623741c.svg"
+  },
+  {
+    "revision": "cd8d7fceea994a098c78b381e4e9916a",
+    "url": "/My-Weather-React/static/media/Tonado.cd8d7fce.svg"
+  }
+]);
